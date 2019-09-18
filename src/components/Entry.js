f8c5eb0f8c69d7eb17aa202ff9682dc75a0ce0e2
Kevin Wei <chenghaowei@gmail.com>
2019-09-18 14:21:09 -0400

    add placeholder image

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -5,7 +5,8 @@ function Entry(props) {
     <div className="Entry">
       <a href={props.website} target="_blank" rel="noopener noreferrer">
         <h3>{props.name}</h3>
-        <p><img src={props.logo} alt={`${props.name} Logo`} /></p>
+         {/* props.logo */}
+        <p><img src={`https://placeimg.com/128/128/tech`} alt={`${props.name} Logo`} /></p>
       </a>
       <p>Job Listings: {props.numJobListings}</p>
     </div>
