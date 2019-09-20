283793c01fa44fa10c7b809e0b13bf7807d37818
Kevin Wei <chenghaowei@gmail.com>
2019-09-20 11:36:28 -0400

    fix gsap tree shaking

--- src/components/Fire/Fire.js
+++ src/components/Fire/Fire.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { TimelineMax } from 'gsap/all';
+import { TimelineMax, CSSPlugin, AttrPlugin } from 'gsap/all';
 import './Fire.css';
 
 export default class Fire extends Component {
   startFireAnim = () => {
+    const plugins = [CSSPlugin, AttrPlugin];
+
     Array.from(document.querySelectorAll('.spark')).forEach(function(s) {
       new TimelineMax({
         repeat: -1,
