06484772b93e6015d81aeb1c61399d0171d60ba0
Kevin Wei <chenghaowei@gmail.com>
2019-11-02 01:18:35 -0400

    comment out heroku backend ping for now

--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// pings the server every 5 minutes
-setInterval(() => fetch(`${config.API_ENDPOINT}`), 5 * 60 * 1000);
+// pings the server every 5 minutes. deploy when ready for demo
+// setInterval(() => fetch(`${config.API_ENDPOINT}`), 5 * 60 * 1000);
